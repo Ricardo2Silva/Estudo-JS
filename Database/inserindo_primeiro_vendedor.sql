b4756bf842986b0f73246c6fbbf5fcdb0bbc9f38
Ricardo2Silva <ricardojonasdasilva60@gmail.com>
2019-12-30 10:31:01 -0300

    subindo estudo database introduçao

---
+++ Database/inserindo_primeiro_vendedor.sql
@@ -0,0 +1,11 @@
+INSERT INTO tbvendedores(
+matricula,nome,percentual_comissao) VALUES ('00233','Jose Geraldo da Fonseca',0.10);
+
+INSERT INTO tbvendedores(
+matricula,nome,percentual_comissao) VALUES ('00235','Marcio Almeida Silva',0.08);
+
+INSERT INTO tbvendedores(
+matricula,nome,percentual_comissao) VALUES ('00236','Claudia Morais',0.08);
+
+
+select * from tbvendedores;
